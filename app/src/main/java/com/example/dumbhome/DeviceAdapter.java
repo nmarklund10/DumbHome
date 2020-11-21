de2340552db096f8a4f1afb84159172f56f1242d
Neil Marklund <nmarklund10@hotmail.com>
2020-11-21 15:27:25 -0500

    Moved sending messages to MessageUtils and added discoery message handler.

--- app/src/main/java/com/example/dumbhome/DeviceAdapter.java
+++ app/src/main/java/com/example/dumbhome/DeviceAdapter.java
@@ -10,102 +10,20 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.dumbhome.messages.A2DDiscoverMessage;
-import com.example.dumbhome.messages.A2DNameMessage;
-import com.example.dumbhome.messages.A2DToggleMessage;
-import com.example.dumbhome.messages.D2AIdentityMessage;
-import com.example.dumbhome.messages.D2AStatusMessage;
 import com.google.android.material.switchmaterial.SwitchMaterial;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.util.List;
-
-import static com.example.dumbhome.messages.A2DNameMessage.NAME_MSG_TYPE;
-import static com.example.dumbhome.messages.A2DToggleMessage.TOGGLE_MSG_TYPE;
+import java.util.ArrayList;
 
 public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.DeviceHolder> {
 
     // Private Members
-    private final List<Device> deviceList;
+    private final ArrayList<Device> deviceList;
     private final Context context;
 
-    private void sendDiscoverMessage() {
-        A2DDiscoverMessage message = new A2DDiscoverMessage();
-        D2AIdentityMessage testMessage =
-                new D2AIdentityMessage(true, false, "Hello", "127.0.0.1");
-        new Thread(new SendAndListen(message)).start();
-        new Thread(() -> {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                DatagramSocket testClient = new DatagramSocket();
-                Log.d("DEBUG", "Client sending packet");
-                testClient.send(testMessage.getPacket());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }).start();
-    }
-
-    private void sendNameMessage(String name, String ipAddress) {
-        A2DNameMessage message = new A2DNameMessage(name, ipAddress);
-        D2AStatusMessage testMessage =
-                new D2AStatusMessage(NAME_MSG_TYPE, true, true, "127.0.0.1");
-        new Thread(new SendAndListen(message)).start();
-        new Thread(() -> {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            try {
-                DatagramSocket testClient = new DatagramSocket();
-                Log.d("DEBUG", "Client sending packet");
-                testClient.send(testMessage.getPacket());
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-        }).start();
-    }
-
-    // Private Functions
-    private void sendToggleMessage(String ipAddress) {
-        A2DToggleMessage message = new A2DToggleMessage(ipAddress);
-        new Thread(new SendAndListen(message)).start();
-//        D2AStatusMessage testMessage =
-//                new D2AStatusMessage(TOGGLE_MSG_TYPE, true, true, "127.0.0.1");
-//        new Thread(() -> {
-//            try {
-//                Thread.sleep(500);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            try {
-//                DatagramSocket testClient = new DatagramSocket();
-//                Log.d("DEBUG", "Client sending packet");
-//                testClient.send(testMessage.getPacket());
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//
-//        }).start();
-    }
-
     private void switchClickListener(Device device, SwitchMaterial deviceSwitch) {
+        // TODO send Toggle
         device.togglePowerState();
         deviceSwitch.setChecked(device.getPowerState());
-        if (device.getPowerState()) {
-//            sendToggleMessage(device.getIpAddress());
-//            sendDiscoverMessage();
-//            sendNameMessage("Hello", "10.31.114.43");
-        }
     }
 
 
@@ -115,7 +33,7 @@ public class DeviceAdapter extends RecyclerView.Adapter<DeviceAdapter.DeviceHold
     }
 
     // Public Functions
-    public DeviceAdapter(List<Device> listItems, Context context) {
+    public DeviceAdapter(ArrayList<Device> listItems, Context context) {
         this.deviceList = listItems;
         this.context = context;
     }
--- app/src/main/java/com/example/dumbhome/SendAndListen.java
+++ app/src/main/java/com/example/dumbhome/SendAndListen.java
@@ -1,16 +1,15 @@
 package com.example.dumbhome;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.util.Log;
 
-import com.example.dumbhome.messages.A2DDiscoverMessage;
-import com.example.dumbhome.messages.D2AIdentityMessage;
-import com.example.dumbhome.messages.D2AStatusMessage;
 import com.example.dumbhome.messages.Message;
+import com.example.dumbhome.messages.MessageUtils;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
@@ -24,26 +23,56 @@ public class SendAndListen implements Runnable {
     private final Message message;
     private final int listeningPort = 17001;
     private final int RECV_SIZE = 4096;
-    private final int TIMEOUT = 2500;
+    private final int TIMEOUT = 2000;
     private byte[] receiverBuffer = new byte[RECV_SIZE];
+    Activity currentActivity;
+    DatagramSocket serverSocket;
 
-    public SendAndListen(Message aMessage) {
+    public SendAndListen(Message aMessage, Activity aActivity) {
         this.message = aMessage;
+        this.currentActivity = aActivity;
     }
 
-    private void handlePacket(DatagramPacket receivedPacket) {
+    private boolean listenForResponse() throws IOException {
+        DatagramPacket receivedPacket =
+                new DatagramPacket(receiverBuffer, receiverBuffer.length);
+        serverSocket.setSoTimeout(TIMEOUT);
+        serverSocket.receive(receivedPacket);
         if (message.msgType == DISCOVER_MSG_TYPE) {
-            // Should get D2AIdentityMessage back
-            D2AIdentityMessage response = D2AIdentityMessage.fromBytes(receivedPacket.getData());
-            Log.d("PROCESS", response.name);
+            MessageUtils.handleIdentityMessage(receivedPacket);
+
         }
         else if (message.msgType == NAME_MSG_TYPE || message.msgType == TOGGLE_MSG_TYPE) {
-            // Should get D2AStatusMessage back
-            D2AStatusMessage response = D2AStatusMessage.fromBytes(receivedPacket.getData());
-            if (response.lastMsgType != message.msgType) {
-                response = null;
+            MessageUtils.handleStatusMessage(receivedPacket, message.msgType);
+            serverSocket.close();
+            if (message.msgType == NAME_MSG_TYPE) {
+                // TODO: update name on list
             }
-            Log.d("PROCESS", String.valueOf(response.relayOn));
+            else if (message.msgType == TOGGLE_MSG_TYPE) {
+                // TODO: update switch
+            }
+            return false;
+        }
+        else {
+            throw new IllegalArgumentException("Unknown message type!");
+        }
+        return true;
+    }
+
+    private void handleTimeout() {
+        serverSocket.close();
+        if (message.msgType == DISCOVER_MSG_TYPE) {
+            currentActivity.runOnUiThread(() -> {
+                // TODO: go to Main Activity
+                Intent intent = new Intent(currentActivity, MainActivity.class);
+                currentActivity.startActivity(intent);
+                currentActivity.finish();
+            });
+        }
+        else {
+            currentActivity.runOnUiThread(() -> {
+                // TODO: Display Error Message
+            });
         }
     }
 
@@ -51,29 +80,15 @@ public class SendAndListen implements Runnable {
     public void run() {
         boolean isRunning = true;
         try {
-            DatagramSocket serverSocket = new DatagramSocket(listeningPort);
-            InetAddress serverAddress = InetAddress.getByName(message.ipAddress);
+            serverSocket = new DatagramSocket(listeningPort);
             // Send Message
             serverSocket.send(message.getPacket());
-            // Listen for Response
             while (isRunning) {
                 try {
-                    DatagramPacket receivedPacket =
-                            new DatagramPacket(receiverBuffer, receiverBuffer.length);
-                    Log.d("DEBUG", "about to wait to receive");
-                    serverSocket.setSoTimeout(TIMEOUT);
-                    serverSocket.receive(receivedPacket);
-                    Log.d("DEBUG", "received packet");
-                    handlePacket(receivedPacket);
-                    Log.d("DEBUG", "processed packet");
+                    isRunning = listenForResponse();
                 } catch (SocketTimeoutException e) {
-                    Log.e("Timeout Exception","UDP Connection:",e);
+                    handleTimeout();
                     isRunning = false;
-                    serverSocket.close();
-                } catch (IOException e) {
-                    Log.e("UDP IO Exception", "error: ", e);
-                    isRunning = false;
-                    serverSocket.close();
                 }
             }
         } catch (SocketException | UnknownHostException e) {
